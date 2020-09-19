31546ea352881fa92194089d37642431d03fc2c8
Andrew Chen <archen2019@gmail.com>
2020-09-19 00:02:10 -0700

    slightly nicer css for chat

--- static/chat.js
+++ static/chat.js
@@ -59,51 +59,7 @@ $(function() {
         });
   }
 
-  chat_connect("sdf");
-
-  // Get an access token for the current user, passing a username (identity)
-/*
-  $.getJSON('/login', function(data) {
-
-
-    // Initialize the Chat client
-    Twilio.Chat.Client.create(data.token).then(client => {
-      console.log('Created chat client');
-      chatClient = client;
-      chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel);
-
-      // when the access token is about to expire, refresh it
-      chatClient.on('tokenAboutToExpire', function() {
-        refreshToken(username);
-      });
-
-      // if the access token already expired, refresh it
-      chatClient.on('tokenExpired', function() {
-        refreshToken(username);
-      });
-
-    // Alert the user they have been assigned a random username
-    username = data.identity;
-    print('You have been assigned a random username of: '
-    + '<span class="me">' + username + '</span>', true);
-
-    }).catch(error => {
-      console.error(error);
-      print('There was an error creating the chat client:<br/>' + error, true);
-      print('Please check your .env file.', false);
-    });
-  });
-    */
-
-  function refreshToken(identity) {
-    console.log('Token about to expire');
-    // Make a secure request to your backend to retrieve a refreshed access token.
-    // Use an authentication mechanism to prevent token exposure to 3rd parties.
-    $.getJSON('/token/' + identity, function(data) {
-      console.log('updated token for chat client');          
-      chatClient.updateToken(data.token);
-    });
-  }
+  chat_connect("dddd");
 
   function createOrJoinGeneralChannel() {
     // Get the general chat channel, which is where all the messages are
